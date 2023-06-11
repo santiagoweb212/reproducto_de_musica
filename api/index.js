59fe1b7df3d428b63d194273fda2d037c967d761
santiagoweb212 <juanusca45@gmail.com>
2023-06-10 22:29:09 -0500

    inicializando servidor

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+const sequelize = require("./config/database");
+require("./relations/relations");
 const app = express();
-const PORT = 3000
-app.listen(PORT, () => {
-    console.log(`Example app listening on port ${PORT}`)
-  })
+const PORT = 3000;
+const main = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync({ force: true });
+    console.log("Connection has been established successfully.");
+    app.listen(PORT, () => {
+      console.log(`Example app listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+main();
