1df5ca75b96a6fe5a8bc4b5ff4dcee60ed8f740e
santiagoweb212 <juanusca45@gmail.com>
2023-06-13 22:40:28 -0500

    controlador usaurio habilitado

--- api/controllers/usuarios.js
+++ api/controllers/usuarios.js
@@ -1,3 +1,21 @@
-const getUsuarios=()=>{}
-const postUsuarios=()=>{}
-module.exports={getUsuarios,postUsuarios}
+const usuarios = require("../models/usuarios");
+const getUsuarios = async (req, res) => {
+  try {
+    return res.status(200).send("hola");
+  } catch (error) {}
+};
+const postUsuarios = async (req, res) => {
+  try {
+    const { nombre, email, password } = req.body;
+    console.log("esty en el controlador");
+    const newUser = await usuarios.create({
+      nombre_usuario: nombre,
+      correo_electronico: email,
+      password,
+    });
+    res.status(200).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
+module.exports = { getUsuarios, postUsuarios };
