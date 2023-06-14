bc1bd236484e02dcb58aaefe048999b096c30d40
santiagoweb212 <juanusca45@gmail.com>
2023-06-13 22:32:34 -0500

    implementacion de cors para evitar error de cros origin

--- api/app.js
+++ api/app.js
@@ -1,4 +1,7 @@
 const express = require('express')
-const routes = require("./routes/index")
+const cors =require('cors')
 const app = express()
-app.use('/',routes)
+app.use(cors())
+app.use(express.json())
+app.use('/api',require('./routes/index'))
+module.exports= app
