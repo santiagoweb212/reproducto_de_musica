b6e13478603a45da78f8dd91eae36dace7591d52
santiagoweb212 <juanusca45@gmail.com>
2023-06-13 22:44:02 -0500

    update

--- api/routes/usuarios.js
+++ api/routes/usuarios.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const { getUsuarios, postUsuarios } = require("../controllers/usuarios");
+const{validateCreate} = require("../validators/usuarios");
 const router = Router();
 router.get("/", getUsuarios);
-router.post("/create-user", postUsuarios);
+router.post("/create-user",validateCreate,postUsuarios)
 module.exports = router;
